2851e66248fcdd5a5aa76c338fb971d1c91a9be6
GermanPR <nexus.parada@hotmail.com>
2025-08-20 13:57:33 +0200

    revert colorscheme change

--- lua/custom/plugins/onedarkpro.lua
+++ lua/custom/plugins/onedarkpro.lua
@@ -2,13 +2,6 @@ return {
 	"olimorris/onedarkpro.nvim",
 	priority = 1000,
 	config = function()
-		require("onedarkpro").setup({
-			colors = {
-				onedark = {
-					bg = "#1c1f36",
-				},
-			},
-		})
 		vim.cmd("colorscheme onedark")
 	end,
 }
