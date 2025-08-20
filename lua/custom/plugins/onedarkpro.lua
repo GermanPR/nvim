e5afb6997e1b413e2d5a5975ae8d06899d687658
GermanPR <nexus.parada@hotmail.com>
2025-08-20 13:52:35 +0200

    Changing atom theme color

--- lua/custom/plugins/onedarkpro.lua
+++ lua/custom/plugins/onedarkpro.lua
@@ -1,7 +1,14 @@
 return {
-  'olimorris/onedarkpro.nvim',
-  priority = 1000,
-  config = function()
-    vim.cmd 'colorscheme onedark'
-  end,
+	"olimorris/onedarkpro.nvim",
+	priority = 1000,
+	config = function()
+		require("onedarkpro").setup({
+			colors = {
+				onedark = {
+					bg = "#1c1f36",
+				},
+			},
+		})
+		vim.cmd("colorscheme onedark")
+	end,
 }
